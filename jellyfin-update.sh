b3142c4c716f023f229011d250199f4a61933cb0
Sibren Wieme <130966911+siwieme@users.noreply.github.com>
2024-11-10 22:15:09 +0100

    Create jellyfin-update.sh

---
+++ jellyfin-update.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Update package list
+echo "Package list updaten..."
+apt update -y
+
+# Upgrade all installed packages
+echo "Alle geïnstalleerde packages upgraden..."
+apt full-upgrade -y
+
+# Remove unnecessary packages
+echo "Overbodige packages verwijderen..."
+apt autoremove -y
+
+# Clean up the local repository of retrieved package files
+echo "Opkuisen lokale repositories van opgevraagde package files..."
+apt clean -y
+apt autoclean -y
+
+echo "Systeem bijgewerkt en opgeschoond!"
+
+echo "Nu Jellyfin heropstarten..."
+systemctl restart jellyfin
+
+echo "Klaar!"
